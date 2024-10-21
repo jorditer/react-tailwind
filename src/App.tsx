c9291e31628c58fca6035d70a9fb329427e303ec
jordit <jordi.terradas6@gmail.com>
2024-10-21 10:58:02 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// import { useState } from 'react'
+
+function App() {
+
+  return (
+   <div className="flex justify-center">
+    <h1 className="mx-auto bg-slate-500 font-bold">Testing</h1>
+   </div> 
+  )
+}
+
+export default App
